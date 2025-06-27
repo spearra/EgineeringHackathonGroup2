a8818ad494b03f50add5fdf2288435b54ee7e919
spearra <66639269+spearra@users.noreply.github.com>
2025-06-27 10:52:12 -0400

    routing example

--- kitchen-helper/src/app/app.routes.ts
+++ kitchen-helper/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ChatForm } from './features/chat-form/chat-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: ChatForm, pathMatch: 'full'}
+];
