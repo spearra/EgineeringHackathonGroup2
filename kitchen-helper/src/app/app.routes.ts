7c1e050b55201cc8715e1422bd8c299c2e5a5e8e
spearra <66639269+spearra@users.noreply.github.com>
2025-06-27 12:45:12 -0400

    add navigation to settings

--- kitchen-helper/src/app/app.routes.ts
+++ kitchen-helper/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ChatForm } from './features/chat-form/chat-form';
 import { Home } from './features/home/home';
+import { SettingsComponent } from './features/settings/settings';
 
 export const routes: Routes = [
     {path: '', component: Home, pathMatch: 'full'},
+    {path: 'settings', component: SettingsComponent},
     {path: 'recipes', component: ChatForm}
 ];
