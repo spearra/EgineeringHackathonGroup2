044b1c995296ec3650c16e3487e33ed926276126
spearra <66639269+spearra@users.noreply.github.com>
2025-06-27 14:54:44 -0400

    rid API key

--- kitchen-helper/src/app/features/chat-form/chat-form.ts
+++ kitchen-helper/src/app/features/chat-form/chat-form.ts
@@ -33,7 +33,7 @@ export class ChatForm implements OnInit{
 
   checkResponse() {
     const client = new OpenAI({
-      apiKey: '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',
+      apiKey: '',
       dangerouslyAllowBrowser: true
     });
     this.callAPI(client);
